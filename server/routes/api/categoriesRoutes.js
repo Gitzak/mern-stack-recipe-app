155df3180711e86c62a58f13b6f60432c2411362
Yassir hakimi <yassirhakimi60@gmail.com>
2024-01-30 00:16:33 +0100

    added images in the route of category by just activating multer _ also check env and decomment cloudinary api

--- server/routes/api/categoriesRoutes.js
+++ server/routes/api/categoriesRoutes.js
@@ -3,15 +3,15 @@ const upload = require("../../middleware/multer");
 const { createCategory, getCategories, getCategoryById, updateCategory, deleteCategory } = require("../../controllers/categoryController");
 const router = express.Router();
 
-// // Create a new Category
-router.post("/", createCategory);
-// // List all the Category
+// Create a new Category
+router.post("/", upload.single("image"), createCategory);
+// List all the Category
 router.get("/", getCategories);
-// // Get Category by ID
+// Get Category by ID
 router.get("/:id", getCategoryById);
-// // Update the Category data
-router.put("/update/:id", updateCategory);
-// // Delete a Category
+// Update the Category data
+router.put("/update/:id", upload.single("image"), updateCategory);
+// Delete a Category
 router.delete("/delete/:id", deleteCategory);
 
 module.exports = router;
