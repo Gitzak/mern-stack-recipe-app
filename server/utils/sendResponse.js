0e88081de2ef5b49addf769cacca222abb4f0b13
GitZak <zakaria.belrhali@gmail.com>
2024-02-09 12:38:42 +0100

    dahsboard

--- server/utils/sendResponse.js
+++
@@ -1,7 +0,0 @@
-exports.sendResponse = (message, status, data) => {
-    const response = {};
-    response.status = status;
-    response.message = message;
-    response.dataResponse = data;
-    return response;
-};
